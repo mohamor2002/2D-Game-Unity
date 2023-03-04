8caa325245d7a6364e06804fc97838c88acc6c64
mohamor2002 <muhammadamor2002@gmail.com>
2023-03-04 22:55:11 +0100

    commit alpha

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float Speed;
+	public Rigidbody2D Rb;
+	Vector3 Change;
+	public float JumpForce; 
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Change.x = Input.GetAxisRaw("Horizontal");
+
+		Rb.MovePosition(Rb.transform.position + Change * Speed * Time.deltaTime);
+		if (Input.GetButton("jump")) ;
+		Debug.Log("jump");
+
+
+		
+
+
+	}
+}
